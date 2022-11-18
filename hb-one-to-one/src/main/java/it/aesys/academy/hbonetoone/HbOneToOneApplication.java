63077d9fbfb78a1f6d02560eb6f0e1125aefa0aa
Enrico Monte <monte.enrico@libero.it>
2022-11-18 15:53:10 +0100

    first commit

---
+++ hb-one-to-one/src/main/java/it/aesys/academy/hbonetoone/HbOneToOneApplication.java
@@ -0,0 +1,13 @@
+package it.aesys.academy.hbonetoone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HbOneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HbOneToOneApplication.class, args);
+	}
+
+}
