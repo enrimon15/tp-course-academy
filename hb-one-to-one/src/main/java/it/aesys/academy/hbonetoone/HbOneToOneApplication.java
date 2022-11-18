19795c54923f17baaa3a95997e278fcb9cbc6592
Enrico Monte <monte.enrico@libero.it>
2022-11-18 16:40:41 +0100

    main

--- hb-one-to-one/src/main/java/it/aesys/academy/hbonetoone/HbOneToOneApplication.java
+++ hb-one-to-one/src/main/java/it/aesys/academy/hbonetoone/HbOneToOneApplication.java
@@ -1,13 +1,19 @@
 package it.aesys.academy.hbonetoone;
 
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class HbOneToOneApplication {
+public class HbOneToOneApplication implements ApplicationRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HbOneToOneApplication.class, args);
 	}
 
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		
+	}
 }
